7441cab0ea947f009e5bfd7c646b593028acaea4
slputra <manishpathak8862@gmail.com>
2021-10-19 22:27:46 +0530

    Initial commit

---
+++ module1.py
@@ -0,0 +1,9 @@
+import datetime
+import sys
+import time
+import datetime
+
+# learning pyspark  ---- comment line ---python does not multiline comment ,you have to use # in each line
+
+print(datetime.date.today())
+print(time.time())
